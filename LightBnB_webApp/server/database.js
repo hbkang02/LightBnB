352910742b4d828b0f7ccc7aafbc68358c6fd9ae
John Kang <hbkang02@gmail.com>
2022-12-17 22:30:29 -0800

    modified all reservations function in database.js to accept reservations from sql database

--- LightBnB_webApp/server/database.js
+++ LightBnB_webApp/server/database.js
@@ -83,7 +83,23 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`
+  SELECT properties.*, reservations.*, avg(property_reviews.rating) AS average_rating
+  FROM reservations
+  LEFT JOIN properties ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT = $2;`, [guest_id, limit])
+  .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+  })
+  .catch ((err) => {
+    console.log("Catch: addUser ", err.message);
+});
 }
 exports.getAllReservations = getAllReservations;
 
